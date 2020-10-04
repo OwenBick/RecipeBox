6036318fe92c2dea829ed44484078d0dc022c93d
Owen Bick <w0726133@myscc.ca>
2020-10-04 16:30:41 -0700

    Initial Commit

---
+++ RecipeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeProject
+//
+//  Created by Owen Bick on 10/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
